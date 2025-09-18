7011341198761c4cce61fb3f3abbacdca8f709a3
majedjh <m.majed.aljajeh@gmail.com>
2025-09-18 16:36:15 +0300

    added screenshots file + minor fixed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import {useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
 import { PostProvider, usePosts } from "./PostProvider";
 
